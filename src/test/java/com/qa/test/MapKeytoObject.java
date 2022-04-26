2860c34ede4a31e3abe467b367aff87e40eb88c5
Sakshi Sachdeva <sakshisachdeva22@gmail.com>
2022-04-26 14:45:24 +0530

    change in testfile

--- src/test/java/com/qa/test/MapKeytoObject.java
+++ src/test/java/com/qa/test/MapKeytoObject.java
@@ -10,38 +10,33 @@ import com.qa.pojo.TMap;
 public class MapKeytoObject {
 
 	public static void main(String[] args) {
+
+		// https://www.youtube.com/watch?v=3hultwSw_nU
 		HashMap<Object, TMap> map1 = new HashMap<Object, TMap>();
-		
-		
+
 		TMap E1 = new TMap(1, "100", "2020");
-		
+
 		TMap E2 = new TMap(2, "200", "2021");
-		
+
 		TMap E3 = new TMap(3, "300", "2022");
-		
+
 		map1.put(E1.getcheckID(), E1);
 		map1.put(E1.getcheckID(), E1);
 		map1.put(E1.getcheckID(), E1);
-	
-		
-		
-		Set<Entry<Object, TMap>> m1= map1.entrySet();
-		
-		for(Map.Entry<Object, TMap> emp : m1)
-			
+
+		Set<Entry<Object, TMap>> m1 = map1.entrySet();
+
+		for (Map.Entry<Object, TMap> emp : m1)
+
 		{
-			//emp.getKey();
-	       String map = emp.getValue().getcheckID();
-	       System.out.println("CHECKID  :  "+  map);
-	       int sum2 = emp.getValue().getsum();
-	     
-	       System.out.println("SUMMMM  :  "+  sum2);
-	       
-			
+			// emp.getKey();
+			String map = emp.getValue().getcheckID();
+			System.out.println("CHECKID  :  " + map);
+			int sum2 = emp.getValue().getsum();
+
+			System.out.println("SUMMMM  :  " + sum2);
+
 		}
 
-		
-		
-		
 	}
 }
